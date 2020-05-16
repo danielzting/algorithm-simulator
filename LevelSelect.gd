382db756caaf7058aa49a732b8186dd4831081a7
Daniel Ting <danielzting@gmail.com>
2020-05-16 12:12:05 -0500

    feat: add basic level loading
    The levels are stored in an array in levels.json and are programatically
    loaded in LevelSelect.gd to separate presentation and logic.

---
+++ LevelSelect.gd
@@ -0,0 +1,11 @@
+extends HBoxContainer
+
+func _ready():
+	var file = File.new()
+	file.open("res://levels.json", file.READ)
+	var levels = parse_json(file.get_as_text())
+	for level in levels:
+		var button = Button.new()
+		button.flat = true
+		button.text = level
+		add_child(button)
