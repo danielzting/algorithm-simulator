e5ddba71e0ad4fdedb59c73b89c11fd232f61acc
Daniel Ting <danielzting@gmail.com>
2020-05-18 13:05:58 -0500

    feat: add selectable levels and previews
    Added two more level names and their descriptions to test out if the
    game would handle multiple levels correctly. Also changed levels.json
    into a dict where the keys are level names and the value is another dict
    with its properties.

--- LevelSelect.gd
+++ LevelSelect.gd
@@ -1,12 +1,21 @@
 extends HBoxContainer
 
+var levels
+
 func _ready():
-	var file = File.new()
-	file.open("res://levels.json", file.READ)
-	var levels = parse_json(file.get_as_text())
+	var descriptions = File.new()
+	descriptions.open("res://levels.json", descriptions.READ)
+	levels = parse_json(descriptions.get_as_text())
+	var first = true
 	for level in levels:
 		var button = Button.new()
-		button.text = level.name
+		button.text = level
 		button.align = Button.ALIGN_LEFT
+		button.connect("focus_entered", self, "_on_Button_focus_changed")
 		$LevelsBorder/Levels.add_child(button)
-		$PreviewBorder/Preview/Info.text = level.documentation
+		if first:
+			button.grab_focus()
+			first = false
+
+func _on_Button_focus_changed():
+	$PreviewBorder/Preview/Info.text = levels[get_focus_owner().text]["about"]
--- MainMenu.gd
+++ MainMenu.gd
@@ -1,4 +1,7 @@
 extends VBoxContainer
 
+func _ready():
+	$StartButton.grab_focus()
+
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://LevelSelect.tscn")
