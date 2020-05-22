01562778906020f5483a4dc14bee4e66c85b67a4
Daniel Ting <danielzting@gmail.com>
2020-05-22 11:55:58 -0500

    feat: add ready screen and timer
    Added a one-second ready screen before the timer starts.

--- scripts/play.gd
+++ scripts/play.gd
@@ -1,8 +1,22 @@
 extends VBoxContainer
 
-func _ready():
-	$HUDBorder/HUD/Level.text = scene.get_param("level")
+var start_time = -1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _ready():
+	var label = Label.new()
+	label.text = "ready..."
+	label.align = Label.ALIGN_CENTER
+	label.set_anchors_preset(Control.PRESET_HCENTER_WIDE)
+	$DisplayBorder/Display.add_child(label)
+	$HUDBorder/HUD/Level.text = scene.get_param("level")
+	match scene.get_param("level"):
+		"BUBBLE SORT":
+			pass
+
+func _process(delta):
+	var elapsed_time = stepify((OS.get_ticks_msec() - start_time) / 1000.0, 0.001)
+	$HUDBorder/HUD/Score.text = "0.000" if start_time == -1 else "%.3f" % elapsed_time
+
+func _on_Timer_timeout():
+	$DisplayBorder/Display.get_child(0).queue_free()
+	start_time = OS.get_ticks_msec()
