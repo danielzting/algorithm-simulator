a74cb50e15ce2c860d6d415b45f9147ff2f50bc9
Daniel Ting <danielzting@gmail.com>
2020-07-29 14:03:31 -0500

    fix: prevent animation race condition
    Save initial positions of array bars so that if the user presses a
    button so fast that one animation begins before the previous was
    finished, the positions wouldn't be messed up.

--- views/array_view.gd
+++ views/array_view.gd
@@ -6,17 +6,17 @@ class_name ArrayView
 extends HBoxContainer
 
 const SWAP_DURATION = 0.1
-const TRANS_TYPE = Tween.TRANS_LINEAR
-const EASE_TYPE = Tween.EASE_IN_OUT
 
 var _tween = Tween.new()
 var _level: ComparisonSort
 var _rects = []
+# Save initial positions to prevent animation race condition
+var _positions = []
+var _unit_height: int
 
 func _init(level):
     """Add colored rectangles."""
     _level = level
-    _level.connect("mistake", self, "_on_Level_mistake")
     _level.array.connect("swapped", self, "_on_ArrayModel_swapped")
     add_child(_level) # NOTE: This is necessary for it to read input
     for i in range(level.array.size):
@@ -26,24 +26,31 @@ func _init(level):
         _rects.append(rect)
         add_child(rect)
     add_child(_tween) # NOTE: This is necessary for it to animate
+    _positions.resize(_level.array.size)
+
+func _ready():
+    _level.connect("mistake", get_parent(), "flash")
+
+func _draw():
+    # HACK: Workaround for resized signal not firing when window resized
+    _unit_height = rect_size.y / _level.array.size
+    for i in range(_rects.size()):
+        _rects[i].rect_scale.y = -1
+        _positions[i] = _rects[i].rect_position
 
 func _process(delta):
     """Update heights of rectangles based on array values."""
-    for i in range(_level.array.size):
-        _rects[i].rect_scale.y = -1 # HACK: Override scale to bypass weird behavior
+    for i in range(_rects.size()):
         _rects[i].color = _level.get_effect(i)
-        _rects[i].rect_size.y = rect_size.y * _level.array.at(i) / _level.array.size
-
-func _on_Level_mistake():
-    """Flash the border red on mistakes."""
-    get_parent().flash()
+        _rects[i].rect_size.y =  _level.array.at(i) * _unit_height
 
 func _on_ArrayModel_swapped(i, j):
     """Produce a swapping animation."""
-    _tween.interpolate_property(_rects[i], "rect_position",
-        null, _rects[j].rect_position, SWAP_DURATION, TRANS_TYPE, EASE_TYPE)
-    _tween.interpolate_property(_rects[j], "rect_position",
-        null, _rects[i].rect_position, SWAP_DURATION, TRANS_TYPE, EASE_TYPE)
+    var time = SWAP_DURATION * (1 + float(j - i) / _level.array.size)
+    _tween.interpolate_property(
+        _rects[i], "rect_position", null, _positions[j], time)
+    _tween.interpolate_property(
+        _rects[j], "rect_position", null, _positions[i], time)
     var temp = _rects[i]
     _rects[i] = _rects[j]
     _rects[j] = temp
