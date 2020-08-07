8971957c7310746530fac8248dc9aeae5b7bcda6
Daniel Ting <danielzting@gmail.com>
2020-08-07 15:58:00 -0500

    refactor: clean up levels.gd

--- scripts/levels.gd
+++ scripts/levels.gd
@@ -16,8 +16,8 @@ func _ready():
         var button = Button.new()
         button.text = level.NAME
         button.align = Button.ALIGN_LEFT
-        button.connect("focus_entered", self, "_on_Button_focus_changed")
-        button.connect("pressed", self, "_on_Button_pressed", [level.NAME])
+        button.connect("focus_entered", self, "_on_Button_focus_entered")
+        button.connect("pressed", self, "_on_Button_pressed", [level])
         $LevelsBorder/Levels.add_child(button)
         # Autofocus last played level
         if GlobalScene.get_param("level") == level:
@@ -31,13 +31,13 @@ func _ready():
     if GlobalScene.get_param("level") == null:
         top_button.grab_focus()
 
-func _on_Button_focus_changed(model=ArrayModel.new(_level.array.size)):
-    _level = _get_level(get_focus_owner().text).new(model)
+func _on_Button_focus_entered(size=_level.array.size):
+    _level = _get_level(get_focus_owner().text).new(ArrayModel.new(size))
     _level.active = false
     $Preview/InfoBorder/Info/About.text = _cleanup(_level.ABOUT)
     $Preview/InfoBorder/Info/Controls.text = _cleanup(_level.CONTROLS)
     # Start over when simulation is finished
-    _level.connect("done", self, "_on_Button_focus_changed")
+    _level.connect("done", self, "_on_Button_focus_entered")
     # Replace old display with new
     for child in $Preview/Display.get_children():
         child.queue_free()
@@ -49,12 +49,12 @@ func _input(event):
     elif event.is_action_pressed("slower"):
         $Timer.wait_time *= 2
     elif event.is_action_pressed("bigger"):
-        _on_Button_focus_changed(ArrayModel.new(min(MAX_SIZE, _level.array.size * 2)))
+        _on_Button_focus_entered(min(MAX_SIZE, _level.array.size * 2))
     elif event.is_action_pressed("smaller"):
-        _on_Button_focus_changed(ArrayModel.new(max(MIN_SIZE, _level.array.size / 2)))
+        _on_Button_focus_entered(max(MIN_SIZE, _level.array.size / 2))
 
-func _on_Button_pressed(name):
-    GlobalScene.change_scene("res://scenes/play.tscn", {"level": _get_level(name)})
+func _on_Button_pressed(level):
+    GlobalScene.change_scene("res://scenes/play.tscn", {"level": level})
 
 func _get_level(name):
     for level in LEVELS:
