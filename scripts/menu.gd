85dfe671e8a9a5bbe637c825f82c7304d93e71db
Daniel Ting <danielzting@gmail.com>
2020-08-28 17:43:04 -0500

    feat: implement help modal in main menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,13 +5,18 @@ var _level = BogoSort.new(ArrayModel.new())
 func _ready():
     $Buttons/Start.grab_focus()
     randomize()
-    $Display.add_child(ArrayView.new(_level))
+    $Display.add_child(ArrayView.new(_level), true)
 
 func _on_Start_pressed():
     GlobalScene.change_scene("res://scenes/levels_redesign.tscn")
 
 func _on_Credits_pressed():
     GlobalScene.change_scene("res://scenes/credits.tscn")
+
+func _on_Help_pressed():
+    $Display/Instructions.show()
+    $Display/HBoxContainer.hide()
+    $Display/Instructions/VBoxContainer/Button.grab_focus()
 
 func _on_Timer_timeout():
     _level.next(null)
@@ -20,3 +25,8 @@ func _input(event):
     # If not in a browser, close the app
     if event.is_action_pressed("ui_cancel") and OS.get_name() != "HTML5":
         get_tree().quit()
+
+func _on_Button_pressed():
+    $Display/Instructions.hide()
+    $Display/HBoxContainer.show()
+    $Buttons/Start.grab_focus()
